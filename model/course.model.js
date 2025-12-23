58baab4b0256c03d11ef95a8acafed29f057b6fb
koma <koma@koma.koma>
2025-12-23 17:53:29 +0500

    otkr

---
+++ model/course.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = {
+  Course: mongoose.model("Course", courseSchema),
+};
---
+++ model/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "user", "teacher"],
+    default: "user",
+  },
+
+  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+});
+
+module.exports = {
+  User: mongoose.model("User", userSchema),
+};
