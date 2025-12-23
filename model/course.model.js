bae5d5f3cb08ca555d56dcb3c7757fafb8c9c6ad
Mee <otkirbekusmonaliyev-code>
2025-12-23 17:44:45 +0500

    course

---
+++ model/course.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+require(process.env.SECRET_KEY)
+
+const courseSchema = mongoose.Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    price: {
+        type: Number, 
+        required: true
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "users"
+    },
+    category: {
+        type: mongoose.Types.ObjectId, 
+        ref: "category"
+    }
+})
+
+const Course = mongoose.model("course", courseSchema)
+
+module.exports = { Course }
